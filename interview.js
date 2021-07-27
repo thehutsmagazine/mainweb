0f958f1dfb6b44796df787339da2469f65a2df70
thehutsmagazine <hutsmagazine@gmail.com>
2021-07-27 16:48:55 +0100

    The Main Upload

---
+++ interview.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+document.querySelector("#text-kgATCZQ > figure:nth-child(8) > figcaption")
+document.querySelector("#text-kgATCZQ > p:nth-child(10)")
+document.querySelector("#text-kgATCZQ")
+document.querySelector("#text-kgATCZQ > p:nth-child(9)")
+document.querySelector("#article-R3VNnfU")
